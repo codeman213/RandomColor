4a736dedab88aa860b72445287c2f72c455825d5
nicarlo-codeman213 <nicarlog@gmail.com>
2020-01-23 18:34:55 -0500

    Updates

---
+++ precache-manifest.78788235941944c716feda31c26897dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f12a772a6b36aabf186a40bfe8ec23",
+    "url": "/RandomColor/index.html"
+  },
+  {
+    "revision": "af7ef4827c25f8a5d974",
+    "url": "/RandomColor/static/js/2.352db49e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RandomColor/static/js/2.352db49e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71541656d6ab4919dce6",
+    "url": "/RandomColor/static/js/main.0cfadc6e.chunk.js"
+  },
+  {
+    "revision": "3a54f34a5a36d617a9ed",
+    "url": "/RandomColor/static/js/runtime-main.13e7cb7d.js"
+  }
+]);
